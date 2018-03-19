a6d395abacd282f41ece475c334551916c39b681
identifier <name@github.com>
2018-03-19 15:14:04 +0100

    Start working

---
+++ ex_1_lab_1.py
@@ -0,0 +1,7 @@
+print("THIS PROGRAM COMPUTE THE SUM OF TWO NUMBERS")
+print("INSERT FIRST NUMBER,PLEASE:")
+a = int(input())
+print("INSERT SECOND NUMBER,PLEASE:")
+b = int(input())
+sum = a+b
+print("THE SUM OF", a, "AND", b, "IS", sum)
---
+++ ex_2_lab_1.py
@@ -0,0 +1,9 @@
+print("THIS PROGRAM COMPUTE MANIPULATES STRINGS")
+print("INPUT A GENERIC STRING")
+str = input()
+lenght = len(str)
+
+if lenght<=2
+    str = ""
+else
+
