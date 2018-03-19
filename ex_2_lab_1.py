cd4a94083c7bbcd7fd45ce3111db96dc956355a9
identifier <name@github.com>
2018-03-19 15:19:16 +0100

    Start working

--- ex_2_lab_1.py
+++ ex_2_lab_1.py
@@ -3,7 +3,9 @@ print("INPUT A GENERIC STRING")
 str = input()
 lenght = len(str)
 
-if lenght<=2
+if lenght<=2:
     str = ""
-else
+else:
+    str = str[0]+str[1]+str[lenght-2]+str[lenght-1]
 
+print(str)
